b59056c68b0733ed5c99177109f89c5f0e49b692
jamesrossjr <james@jamesrossjr.com>
2025-04-04 10:45:22 -0500

    Resolved missing pages

---
+++ pages/contact.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+// Import necessary modules and components
+</script>
+
+<style scoped>
+/* Add any scoped styles here */
+</style>
